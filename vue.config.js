4aef673d57c8cd96ebec82f2ad7ad430336b8be5
moyangzhen <15711476734@163.com>
2022-12-13 20:46:42 +0800

    add publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable quotes */
 module.exports = {
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "circle-demo_self" : "/"
 }
