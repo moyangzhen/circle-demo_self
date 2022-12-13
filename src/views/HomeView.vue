9cd6e76ed2feea4e11a4a8511059bdea527239dd
moyangzhen <15711476734@163.com>
2022-12-13 20:52:24 +0800

    add publicpath /a

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="楚盼盼是个傻逼" />
   </div>
 </template>
 
